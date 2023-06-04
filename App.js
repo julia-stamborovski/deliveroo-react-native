fc2d5323e5ddf510e2e12f0cb7df3ebcd5d774a8
Julia Stamborovski <juliashizuko66@gmail.com>
2023-06-04 17:03:27 -0300

    home page, componenets-categories and fix

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,15 @@
-import { SafeAreaView, View, Text, Image } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
-
+import { SafeAreaView, View, Text, Image, TextInput, ScrollView } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+    UserIcon,
+    ChevronDownIcon,
+    MagnifyingGlassIcon,
+    AdjustmentsVerticalIcon
+} from 'react-native-heroicons/outline';
+import Categories from '../components/categories';
 const HomeScreen = () => {
-
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
 
     useLayoutEffect(() => {
@@ -15,24 +20,40 @@ const HomeScreen = () => {
 
 
   return (
-    <SafeAreaView>
-      <Text className='text-pink-500'>
-        {/* header */}
-        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
-            <Image 
-            source={{
-                    uri:"https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450",
-                }}
-            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
-            />
-            <View>
-                <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
-                <Text className='font-bold text-xl'>Current Location</Text>
-            </View>
-        </View>
-      </Text>
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView className="bg-white pt-5">
+    <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
+      <Image
+        source={{
+          uri: "https://links.papareact.com/wru",
+        }}
+        className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+      />
+      <View className="flex-1">
+        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+        <Text className="font-bold text-xl">
+          Current Location
+          <ChevronDownIcon size={20} color="#00CCBB" />
+        </Text>
+      </View>
+      <UserIcon size={35} color="#00CCBB" />
+    </View>
+    {/* search */}
+    <View className="flex-row items-center space-x-2 pb-2 mx-4">
+      <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+        <MagnifyingGlassIcon color="gray" size={20} />
+        <TextInput
+          placeholder="Restaurants and cuisines"
+          keyboardType="default"
+        />
+      </View>
+      <AdjustmentsVerticalIcon color="#00CCBB" />
+    </View>
+
+    <ScrollView className="bg-gray-100">
+        <Categories />
+    </ScrollView>
+  </SafeAreaView>
+  );
+};
 
 export default HomeScreen
